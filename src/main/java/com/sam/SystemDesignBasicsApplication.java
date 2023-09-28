060612cfc766920022e0056f8b713529af10e98b
srgir <saumya.giri07@gmail.com>
2023-09-28 21:33:04 +0530

    Initial commit

---
+++ src/main/java/com/sam/SystemDesignBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.sam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDesignBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemDesignBasicsApplication.class, args);
+	}
+
+}
